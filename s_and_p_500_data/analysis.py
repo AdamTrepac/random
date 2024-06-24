9648f5475b0374a68085efc610e2cc92bc5177e8
adam t <adam.trepac@gmail.com>
2024-06-24 13:46:10 -0700

    add rolling mean to initial dataframe

--- s_and_p_500_data/analysis.py
+++ s_and_p_500_data/analysis.py
@@ -1,12 +1,18 @@
 import pandas as pd
+import matplotlib as plt
 
 df = pd.read_csv("SandP500.csv")
 
 print(df.head(5))
+print(df.describe())
 # print(df.corr())
 
 # roll_avg = df['Close/Last']mavg = close.
-close_df = df["Close/Last"].to_frame()
-print(close_df.head(5))
-close_df["roll_avg"] = close_df["Close/Last"].rolling(4).mean()
-print(close_df)
+
+# close_df = df["Close/Last"].to_frame()
+# print(close_df.head(5))
+# close_df["roll_avg"] = close_df["Close/Last"].rolling(4).mean()
+# print(close_df)
+
+df["roll_avg"] = df["Close/Last"].rolling(4).mean()
+print(df)
